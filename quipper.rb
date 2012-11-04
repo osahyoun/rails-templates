a7ce50bb7eba1d3f14912c322f3745a7e7eb85bf
Omar Sahyoun <osahyoun@gmail.com>
2012-11-04 17:24:07 +0000

    Injects line into spec helper, requiring factory girl rails.

--- quipper.rb
+++ quipper.rb
@@ -33,6 +33,10 @@ run "bundle install"
 
 generate('rspec:install')
 
+inject_into_file 'spec/spec_helper.rb', after: 'require File.expand_path("../../config/environment", __FILE__)' do
+  "\nrequire 'factory_girl_rails'"
+end
+
 file 'spec/factories.rb', <<-CODE
 require 'schema/factories/seed'
 CODE
